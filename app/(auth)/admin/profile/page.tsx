3c06e869b76ca0e67bd5bdba8f195bba12206fbc
Rh4ndon <daverhandon@gmail.com>
2024-06-20 14:25:56 +0800

    Update get employee to new end point

--- app/(auth)/admin/profile/page.tsx
+++ app/(auth)/admin/profile/page.tsx
@@ -14,11 +14,7 @@ export default async function Page() {
 
   if (!accessToken) redirect("/login");
 
-  const userID = session.user.id;
-
-  const ID = String(userID).slice(-2);
-
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL!}employee/${ID}`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL!}employee/current`, {
     headers: {
       method: "GET",
       Authorization: `Bearer ${accessToken}`,
@@ -27,9 +23,7 @@ export default async function Page() {
 
   const employee: EmployeeProfile[] = await res.json();
 
-  console.log(employee);
-  console.log(ID);
-
+  
   return (
 
     <div className="container mx-auto p-4 flex justify-center items-center relative">
